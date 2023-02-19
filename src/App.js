12d7bfc56105c347cc1e85ef1b768f0145bd7f99
biainthew <119650572+biainthew@users.noreply.github.com>
2023-02-20 01:24:23 +0900

    delete detail

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Main from './components/Main';
 import { Route, Routes } from 'react-router';
-import Detail from './components/Detail';
 
 const App = () => {
     return (
