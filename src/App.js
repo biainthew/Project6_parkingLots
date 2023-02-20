1636dd33a2d26e980bee1eaac05310b0a5e5275b
biainthew <119650572+biainthew@users.noreply.github.com>
2023-02-20 16:09:46 +0900

    ddddd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Main from './components/Main';
-import { Route, Routes } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Main />}></Route>
+                <Route path="/Project6_parkingLots/" element={<Main />}></Route>
             </Routes>
         </BrowserRouter>
     );
